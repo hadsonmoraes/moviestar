424dde9f00355176a061c214a21d43b90a5c0122
Hadson Moraes <hadsonmartins10@gmail.com>
2024-05-07 18:37:59 -0400

    iniciando o estilo

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ require_once("db.php")
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>MovieStar</title>
-  <link rel="shortcut icon" href="img/moviestar.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico" type="image/x-icon">
   <!-- CSS -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
   <!-- BOOTSTRAP -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- FONT AWESOME -->
@@ -21,8 +21,64 @@ require_once("db.php")
 </head>
 
 <body>
-  <h1>testando MovieStar</h1>
-  <i class="far fa-plus-square"></i>
+  <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+      <a href="<?= $BASE_URL ?>" class="navbar-brand">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
+        <span id="moviestar-title">MovieStar</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Togglenavigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <form action="" method="get" id="search-form" class="d-flex my-2 my-lg-0">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        <button type="submit" class="btn my-2 my-sm-0">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar /Cadastrar</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <div id="main-container" class="container-fluid">
+    <h1>Corpo do site </h1>
+  </div>
+  <footer id="footer">
+    <div class="social-container">
+      <ul>
+        <li>
+          <a href="#">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-instagram-square"></i>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fab fa-youtube-square"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="footer-links-container">
+      <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar crítica</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+      </ul>
+    </div>
+    <p>&copy; 2024 Hora de Codar</p>
+  </footer>
+
   <!-- BOOTSTRAP JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.js" integrity="sha512-lsA4IzLaXH0A+uH6JQTuz6DbhqxmVygrWv1CpC/s5vGyMqlnP0y+RYt65vKxbaVq+H6OzbbRtxzf+Zbj20alGw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
